816e512dc4b60af9f325b2b9b3c06061f5e315e8
pankotek <ignat2404@gmail.com>
2020-06-04 17:13:29 +0300

    Added test product page

--- pages/locators.py
+++ pages/locators.py
@@ -9,3 +9,7 @@ class LoginPageLocators:
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
     LOGIN_URL = (By.CSS_SELECTOR, "#login_link")
+
+
+class ProductPageLocators:
+    ADD_TO_BASKET = (By.CSS_SELECTOR, ".btn-add-to-basket")
---
+++ pages/product_page.py
@@ -0,0 +1,27 @@
+from .base_page import BasePage
+from .locators import ProductPageLocators
+from selenium.common.exceptions import NoAlertPresentException
+import math
+
+
+class ProductPage(BasePage):
+    def add_to_basket(self):
+        add_to_basket_button = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET)
+        add_to_basket_button.click()
+        self.solve_quiz_and_get_code()
+
+    def solve_quiz_and_get_code(self):
+        alert = self.browser.switch_to.alert
+        x = alert.text.split(" ")[2]
+        answer = str(math.log(abs((12 * math.sin(float(x))))))
+        alert.send_keys(answer)
+        alert.accept()
+        try:
+            alert = self.browser.switch_to.alert
+            alert_text = alert.text
+            print(f"Your code: {alert_text}")
+            alert.accept()
+        except NoAlertPresentException:
+            print("No second alert presented")
+
+
