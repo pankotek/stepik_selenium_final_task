862f32cd1671ae6d2d13579f0c55d27f7cd7a752
pankotek <ignat2404@gmail.com>
2020-06-05 13:25:22 +0300

    Refactored product page

--- pages/product_page.py
+++ pages/product_page.py
@@ -1,18 +1,11 @@
 from .base_page import BasePage
 from .locators import ProductPageLocators
-from selenium.common.exceptions import NoAlertPresentException
-import math
 
 
 class ProductPage(BasePage):
     def add_to_basket(self):
-        name = self.get_name()
-        price = self.get_price()
         add_to_basket_button = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET)
         add_to_basket_button.click()
-        self.solve_quiz_and_get_code()
-        self.assert_name(name)
-        self.assert_price(price)
 
     def get_name(self):
         return self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
@@ -28,16 +21,10 @@ class ProductPage(BasePage):
         added_price = self.browser.find_element(*ProductPageLocators.PRODUCT_ADDED_PRICE).text
         assert added_price == price, "Price is incorrect"
 
-    def solve_quiz_and_get_code(self):
-        alert = self.browser.switch_to.alert
-        x = alert.text.split(" ")[2]
-        answer = str(math.log(abs((12 * math.sin(float(x))))))
-        alert.send_keys(answer)
-        alert.accept()
-        try:
-            alert = self.browser.switch_to.alert
-            alert_text = alert.text
-            print(f"Your code: {alert_text}")
-            alert.accept()
-        except NoAlertPresentException:
-            print("No second alert presented")
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.ADD_TO_CART_MESSAGES), \
+            "Success message is presented, but should not be"
+
+    def success_message_disappeared(self):
+        assert self.is_disappeared(*ProductPageLocators.ADD_TO_CART_MESSAGES), \
+            "Success message is presented, but should not be"
--- test_product_page.py
+++ test_product_page.py
@@ -4,18 +4,32 @@ import pytest
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
 
 
-@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
-                                  pytest.param("http://selenium1py.pythonanywhere.com/catalogue/"
-                                               "coders-at-work_207/?promo=offer7", marks=pytest.mark.xfail),
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
-def test_guest_can_add_product_to_basket(browser, link):
+def test_guest_can_add_product_to_basket(browser):
     page = ProductPage(browser, link)
     page.open()
+    name = page.get_name()
+    price = page.get_price()
     page.add_to_basket()
+    page.solve_quiz_and_get_code()
+    page.assert_name(name)
+    page.assert_price(price)
+
+
+# def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+#     page = ProductPage(browser, link)
+#     page.open()
+#     page.add_to_basket()
+#     page.should_not_be_success_message()
+#
+#
+# def test_guest_cant_see_success_message(browser):
+#     page = ProductPage(browser, link)
+#     page.open()
+#     page.should_not_be_success_message()
+#
+#
+# def test_message_disappeared_after_adding_product_to_basket(browser):
+#     page = ProductPage(browser, link)
+#     page.open()
+#     page.add_to_basket()
+#     page.success_message_disappeared()
