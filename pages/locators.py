4bb4c493f322b8046025cba825c0bb3ebf5551ee
pankotek <ignat2404@gmail.com>
2020-06-09 17:19:02 +0300

    Created user tests

--- pages/locators.py
+++ pages/locators.py
@@ -5,6 +5,9 @@ class LoginPageLocators:
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
     LOGIN_URL = (By.CSS_SELECTOR, "#login_link")
+    REGISTER_EMAIL = (By.CSS_SELECTOR, "#id_registration-email")
+    REGISTER_PASSWORD = (By.CSS_SELECTOR, "#id_registration-password1")
+    REGISTER_CONFIRM_PASSWORD = (By.CSS_SELECTOR, "#id_registration-password2")
 
 
 class ProductPageLocators:
@@ -19,6 +22,7 @@ class ProductPageLocators:
 class BasePageLocators:
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     BASKET_LINK = (By.CSS_SELECTOR, 'a[href$="basket/"]')
+    USER_ICON = (By.CSS_SELECTOR, ".icon-user")
 
 
 class BasketPageLocators:
--- test_product_page.py
+++ test_product_page.py
@@ -2,6 +2,7 @@ from .pages.product_page import ProductPage
 from .pages.login_page import LoginPage
 from .pages.basket_page import BasketPage
 import pytest
+import time
 
 
 def test_guest_can_add_product_to_basket(browser):
@@ -64,3 +65,30 @@ def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     page.go_to_basket()
     page.basket_is_empty()
     page.basket_is_empty_text()
+
+
+class TestUserAddToBasketFromProductPage:
+    pytest.fixture(scope='function', autouse=True)
+    def setup(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/accounts/login/"
+        self.page = LoginPage(browser, link)
+        self.page.open()
+        self.page.register_new_user(str(time.time()) + "@fakemail.org", "12345678a")
+        self.page.should_be_authorized_user()
+
+    def test_user_cant_see_success_message(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
+        page = ProductPage(browser, link)
+        page.open()
+        page.should_not_be_success_message()
+
+    def test_user_can_add_product_to_basket(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+        page = ProductPage(browser, link)
+        page.open()
+        name = page.get_name()
+        price = page.get_price()
+        page.add_to_basket()
+        page.solve_quiz_and_get_code()
+        page.assert_name(name)
+        page.assert_price(price)
